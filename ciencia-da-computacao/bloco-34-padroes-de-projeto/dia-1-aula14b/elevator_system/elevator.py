4fefa3970444775c148968e7ac4f7036e495026a
jpcp0614 <jpcp0614@gmail.com>
2022-07-05 19:06:39 -0300

    Implementa o status com uma classe enum

--- ciencia-da-computacao/bloco-34-padroes-de-projeto/dia-1-aula14b/elevator_system/elevator.py
+++ ciencia-da-computacao/bloco-34-padroes-de-projeto/dia-1-aula14b/elevator_system/elevator.py
@@ -1,5 +1,15 @@
+from enum import Enum
+
+
 class Elevator():
     def __init__(self):
         self.current_floor = 0
         self.door_is_open = False
-        self._status = 0
+        self._status = ElevatorStatus.STOPPED
+
+
+class ElevatorStatus(Enum):
+    STOPPED = 0
+    GOING_UP = 1
+    GOING_DOWN = 2
+    LOCKED = 3
--- ciencia-da-computacao/bloco-34-padroes-de-projeto/dia-1-aula14b/tests/test_elevator.py
+++ ciencia-da-computacao/bloco-34-padroes-de-projeto/dia-1-aula14b/tests/test_elevator.py
@@ -1,4 +1,4 @@
-from elevator_system.elevator import Elevator
+from elevator_system.elevator import Elevator, ElevatorStatus
 
 
 def test_create_elevator():
@@ -7,3 +7,5 @@ def test_create_elevator():
     assert elevator.current_floor == 0
 
     assert elevator.door_is_open is False
+
+    assert elevator._status == ElevatorStatus.STOPPED
