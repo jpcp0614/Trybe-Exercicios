be10b3be671637d8970e68b2cab90a401c5e452f
jpcp0614 <jpcp0614@gmail.com>
2022-07-05 16:31:07 -0300

    Dicas01

--- ciencia-da-computacao/bloco-34-padroes-de-projeto/dia-1-poo-em-python/dicas01.py
+++ ciencia-da-computacao/bloco-34-padroes-de-projeto/dia-1-poo-em-python/dicas01.py
@@ -32,3 +32,53 @@ class ContaInvestimento(Conta):
 
     def adicioneJuros(self):
         self.saldo += self.saldo * self.taxaJuros / 100
+
+
+class Funcionario:
+    def __init__(self, nome):
+        self.__nome = nome
+
+    def retorna_nome(self):
+        return self.__nome
+
+
+class Empresa:
+    func = []
+
+    def __init__(self):
+        print('Empresa Fictícia em funcionamento')
+
+    def funcionar(self):
+        while True:
+            print('1. Contratar')
+            print('2. Exibir lista de funcionários')
+            print('0. Sair...')
+            op = int(input())
+
+            if op == 1:
+                self.contratar()
+            elif op == 2:
+                self.exibir()
+            elif op == 0:
+                self.sair()
+            else:
+                print('Opção inválida')
+                break
+
+    def contratar(self):
+        nome = input('Nome: ')
+        self.func.append(Funcionario(nome))
+
+    def exibir(self):
+        for funcionario in self.func:
+            print(f'''
+            {funcionario.retorna_nome()}
+            ''')
+
+    def sair(self):
+        print('Empresa Fictícia encerrada')
+        exit()
+
+
+empresa = Empresa()
+empresa.funcionar()
