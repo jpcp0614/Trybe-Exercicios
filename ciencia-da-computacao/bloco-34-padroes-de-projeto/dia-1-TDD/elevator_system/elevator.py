565a6899e075c49014ed99d3a52d7d90c208ee53
jpcp0614 <jpcp0614@gmail.com>
2022-07-06 20:11:18 -0300

    métodos de move's

--- ciencia-da-computacao/bloco-34-padroes-de-projeto/dia-1-TDD/elevator_system/elevator.py
+++ ciencia-da-computacao/bloco-34-padroes-de-projeto/dia-1-TDD/elevator_system/elevator.py
@@ -16,3 +16,24 @@ class Elevator():
 
     def get_status(self):
         return self.__status
+
+    def move(self, destination_floor):
+        if destination_floor == self.current_floor:
+            self.door_is_open = True
+            self.__status = ElevatorStatus.STOPPED
+            return
+        self.door_is_open = False
+
+        if destination_floor > self.current_floor:
+            self.move_up(destination_floor)
+        else:
+            self.move_down(destination_floor)
+        self.current_floor = destination_floor
+
+    def move_up(self, destination_floor):
+        self.__status = ElevatorStatus.GOING_UP
+        self.current_floor = destination_floor
+
+    def move_down(self, destination_floor):
+        self.__status = ElevatorStatus.GOING_DOWN
+        self.current_floor = destination_floor
