d5ca97feb2a2041d94d2e48771e42be83429d825
jpcp0614 <jpcp0614@gmail.com>
2022-07-06 12:16:56 -0300

    Round 1

--- ciencia-da-computacao/bloco-34-padroes-de-projeto/dia-1-poo-em-python/mentoria/main.py
+++ ciencia-da-computacao/bloco-34-padroes-de-projeto/dia-1-poo-em-python/mentoria/main.py
@@ -1,4 +1,17 @@
 from personagem import Personagem
+from jedi_sith import Jedi, Sith
+
 
 personagem1 = Personagem('Padmé', 'Humano', 50, 160, 50)
-print(personagem1.nome)
+darth_vader = Sith('Darth Vader', 'Humano', 130, 200, 130)
+luke = Jedi('Luke', 'Humano', 80, 170, 100)
+
+print('Personagens:')
+print(f'{personagem1.nome}')
+print(f'{darth_vader.nome}')
+print(f'{luke.nome}\n')
+
+
+print('Round 1:')
+print(f'{darth_vader.nome} ataca {luke.nome}')
+darth_vader.atacar(luke)
