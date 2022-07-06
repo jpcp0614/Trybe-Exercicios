bf9dd76dfa3c2fe1a5d785e53e89800125ed6e4c
jpcp0614 <jpcp0614@gmail.com>
2022-07-05 23:28:37 -0300

    Refatora o move

--- ciencia-da-computacao/bloco-34-padroes-de-projeto/dia-1-aula14b/elevator_system/elevator.py
+++ ciencia-da-computacao/bloco-34-padroes-de-projeto/dia-1-aula14b/elevator_system/elevator.py
@@ -8,19 +8,24 @@ class Elevator():
         self._status = ElevatorStatus.STOPPED
 
     def move(self, destination_floor):
-
         if destination_floor == self.current_floor:
             self.door_is_open = True
             self._status = ElevatorStatus.STOPPED
             return
-
         self.door_is_open = False
 
         if destination_floor > self.current_floor:
-            self._status = ElevatorStatus.GOING_UP
+            self.move_up(destination_floor)
         else:
-            self._status = ElevatorStatus.GOING_DOWN
+            self.move_down(destination_floor)
+        self.current_floor = destination_floor
 
+    def move_up(self, destination_floor):
+        self._status = ElevatorStatus.GOING_UP
+        self.current_floor = destination_floor
+
+    def move_down(self, destination_floor):
+        self._status = ElevatorStatus.GOING_DOWN
         self.current_floor = destination_floor
 
 
