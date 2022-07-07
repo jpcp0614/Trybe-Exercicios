6b4a51ece7ce427e95ed56de2e3a805d86d262b3
jpcp0614 <jpcp0614@gmail.com>
2022-07-07 19:39:03 -0300

    Calculator para português e inglês, dinamicamente

--- ciencia-da-computacao/bloco-34-padroes-de-projeto/dia-3-padroes-decorator-observer-factory/decorator/calculator.py
+++ ciencia-da-computacao/bloco-34-padroes-de-projeto/dia-3-padroes-decorator-observer-factory/decorator/calculator.py
@@ -3,37 +3,36 @@ class Calculator:
         return x + y
 
 
-class DecoratedCalculatorBR:
-    def __init__(self, calculator: Calculator):
+class Language():
+    BR = {
+      'um': 1, 'dois': 2, 'tres': 3, 'quatro': 4, 'cinco': 5,
+      'seis': 6, 'sete': 7, 'oito': 8, 'nove': 9, 'zero': 0,
+    }
+
+    EN = {
+      'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5,
+      'six': 6, 'seven': 7, 'eight': 8, 'nine': 9, 'zero': 0,
+    }
+
+    @classmethod
+    def check(cls, language, number):
+        if language == 'en':
+            return cls.EN.get(number)
+        elif language == 'br':
+            return cls.BR.get(number)
+        else:
+            raise ValueError('Language not supported')
+
+
+class DecoratedCalculator:
+    def __init__(self, calculator: Calculator, language):
         self.calculator = calculator
+        self.language = language
 
     def convert_number(self, number):
         if not isinstance(number, str):
             return number
-
-        return {
-          'um': 1, 'dois': 2, 'tres': 3, 'quatro': 4, 'cinco': 5,
-          'seis': 6, 'sete': 7, 'oito': 8, 'nove': 9, 'zero': 0,
-        }.get(number)
-
-    def soma(self, x, y):
-        x = self.convert_number(x)
-        y = self.convert_number(y)
-        return self.calculator.soma(x, y)
-
-
-class DecoratedCalculatorEN:
-    def __init__(self, calculator: Calculator):
-        self.calculator = calculator
-
-    def convert_number(self, number):
-        if not isinstance(number, str):
-            return number
-
-        return {
-          'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5,
-          'six': 6, 'seven': 7, 'eight': 8, 'nine': 9, 'zero': 0,
-        }.get(number)
+        return Language.check(self.language, number)
 
     def soma(self, x, y):
         x = self.convert_number(x)
@@ -42,10 +41,10 @@ class DecoratedCalculatorEN:
 
 
 calculadora = Calculator()
-print(calculadora.soma(1, 2))
+# print(calculadora.soma(1, 2))
 
-calculadoraDecoradaBR = DecoratedCalculatorBR(calculadora)
-print(calculadoraDecoradaBR.soma('um', 'dois'))
+calculadoraDecorada = DecoratedCalculator(calculadora, 'en')
+print(calculadoraDecorada.soma('one', 'two'))
 
-calculadoraDecoradaEN = DecoratedCalculatorEN(calculadora)
-print(calculadoraDecoradaEN.soma('one', 'two'))
+calculadoraDecorada = DecoratedCalculator(calculadora, 'br')
+print(calculadoraDecorada.soma('um', 'dois'))
