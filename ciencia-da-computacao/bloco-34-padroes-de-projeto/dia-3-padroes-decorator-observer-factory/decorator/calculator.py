adaa79a34ef4eaae242050261e52e8c71e20ff6c
jpcp0614 <jpcp0614@gmail.com>
2022-07-07 18:59:19 -0300

    Calculator em inglês

--- ciencia-da-computacao/bloco-34-padroes-de-projeto/dia-3-padroes-decorator-observer-factory/decorator/calculator.py
+++ ciencia-da-computacao/bloco-34-padroes-de-projeto/dia-3-padroes-decorator-observer-factory/decorator/calculator.py
@@ -22,8 +22,30 @@ class DecoratedCalculatorBR:
         return self.calculator.soma(x, y)
 
 
+class DecoratedCalculatorEN:
+    def __init__(self, calculator: Calculator):
+        self.calculator = calculator
+
+    def convert_number(self, number):
+        if not isinstance(number, str):
+            return number
+
+        return {
+          'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5,
+          'six': 6, 'seven': 7, 'eight': 8, 'nine': 9, 'zero': 0,
+        }.get(number)
+
+    def soma(self, x, y):
+        x = self.convert_number(x)
+        y = self.convert_number(y)
+        return self.calculator.soma(x, y)
+
+
 calculadora = Calculator()
 print(calculadora.soma(1, 2))
 
-calculadoraDecorada = DecoratedCalculatorBR(calculadora)
-print(calculadoraDecorada.soma('um', 'dois'))
+calculadoraDecoradaBR = DecoratedCalculatorBR(calculadora)
+print(calculadoraDecoradaBR.soma('um', 'dois'))
+
+calculadoraDecoradaEN = DecoratedCalculatorEN(calculadora)
+print(calculadoraDecoradaEN.soma('one', 'two'))
