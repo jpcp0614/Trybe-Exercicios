0a9d408bda821c13e15ac3cf417f267eb79d4e33
jpcp0614 <jpcp0614@gmail.com>
2022-07-08 12:21:27 -0300

    Class RandomSellerWithInterns

--- ciencia-da-computacao/bloco-34-padroes-de-projeto/dia-3-aula14b/decorator/rodizio.py
+++ ciencia-da-computacao/bloco-34-padroes-de-projeto/dia-3-aula14b/decorator/rodizio.py
@@ -30,10 +30,28 @@ class RandomSeller:
         return seller
 
 
+class RandomSellerWithInterns:
+    def __init__(self, seller_team):
+        # lista de estagiários
+        list_interns = ['Gustavo', 'Jorge', 'Amanda']
+        self.seller_team = seller_team
+        self.seller_team += list_interns
+
+    def next_seller(self):
+        seller = random.choice(self.seller_team)
+        print(f'Vendedor: {seller}')
+        return seller
+
+
 if __name__ == '__main__':
     seller_team = SellerTeam()
     list_sellers = seller_team.people
     print(list_sellers)
 
+    print('Pessoas Vendedoras')
     random_seller = RandomSeller(list_sellers)
     random_seller.next_seller()
+
+    print('Pessoas Vendedoras + Estagiários')
+    random_seller_with_interns = RandomSellerWithInterns(list_sellers)
+    random_seller_with_interns.next_seller()
