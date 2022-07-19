87af53dcd8d27d5327fd52719a1549181b70b6f5
jpcp0614 <jpcp0614@gmail.com>
2022-07-18 21:12:07 -0300

    Criar o banco e as coleções

--- ciencia-da-computacao/bloco-35-redes-e-raspagem-de-dados/dia-2-raspagem-de-dados/db/connection.py
+++ ciencia-da-computacao/bloco-35-redes-e-raspagem-de-dados/dia-2-raspagem-de-dados/db/connection.py
@@ -6,3 +6,12 @@ from pymongo import MongoClient
 # client = MongoClient('mongodb://localhost:27017')
 
 client = MongoClient()
+
+# o banco será criado, caso não exista
+db = client.catalogue
+
+# a coleção books será criada se não existir
+students = db.books
+
+# fecha a conexão com o db
+client.close()
