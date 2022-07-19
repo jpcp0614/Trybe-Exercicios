b457e4ef8d3d7e5a8a41a2b3f74f951c927b7aa0
jpcp0614 <jpcp0614@gmail.com>
2022-07-18 23:39:15 -0300

    cria a parse_quotes_html

--- ciencia-da-computacao/bloco-35-redes-e-raspagem-de-dados/dia-2-aula15b/quotes_scraper.py
+++ ciencia-da-computacao/bloco-35-redes-e-raspagem-de-dados/dia-2-aula15b/quotes_scraper.py
@@ -1,5 +1,5 @@
 import requests
-# from parsel import Selector
+from parsel import Selector
 import time
 
 
@@ -13,7 +13,21 @@ def fetch_html(url: str) -> str:
     return res.text
 
 
+def parse_quotes_html(html_content: str) -> list[dict]:
+    selector = Selector(html_content)
+    result = list()
+    for quote in selector.css('div.col-md-8 > div.quote'):
+        content = quote.css('span.text ::text').get()
+        author = quote.css('span small.author ::text').get()
+        tags = quote.css('div.tags a.tag ::text').getall()
+        quote_data = {"content": content, "author": author, "tags": tags}
+        result.append(quote_data)
+    return result
+
+
 def scrape_all_quotes() -> list[dict]:
     BASE_URL = 'https://quotes.toscrape.com/'
     quotes_html = fetch_html(BASE_URL)
-    return quotes_html
+    quotes_data = parse_quotes_html(quotes_html)
+
+    return quotes_data
