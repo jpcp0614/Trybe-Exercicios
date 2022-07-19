e56e103ce06b59e57826ba2ce15769c9f89f6da4
jpcp0614 <jpcp0614@gmail.com>
2022-07-19 00:46:13 -0300

    adiciona parse_next_url e id automático com a uuid

--- ciencia-da-computacao/bloco-35-redes-e-raspagem-de-dados/dia-2-aula15b/quotes_scraper.py
+++ ciencia-da-computacao/bloco-35-redes-e-raspagem-de-dados/dia-2-aula15b/quotes_scraper.py
@@ -2,14 +2,15 @@ from datetime import datetime
 import requests
 from parsel import Selector
 import time
+import uuid
 
 
 def fetch_html(url: str) -> str:
     time.sleep(1)  # espera um pouco pra fazer a próxima request
     try:
-        res = requests.get(url, timeout=3)  # se demorar mais de 3s, quebra
+        res = requests.get(url, timeout=10)  # se demorar mais de 3s, quebra
         res.raise_for_status()
-    except (requests.HTTPError, TimeoutError):
+    except requests.HTTPError:
         return ""
     return res.text
 
@@ -22,6 +23,7 @@ def parse_quotes_html(html_content: str) -> list[dict]:
         author = quote.css('span small.author ::text').get()
         tags = quote.css('div.tags a.tag ::text').getall()
         quote_data = {
+          "id": uuid.uuid4().hex,
           "content": content,
           "author": author,
           "tags": tags,
@@ -31,9 +33,22 @@ def parse_quotes_html(html_content: str) -> list[dict]:
     return result
 
 
+def parse_next_url(html_content: str) -> str:
+    selector = Selector(html_content)
+    return selector.css('ul.pager li.next a ::attr(href)').get()
+
+
 def scrape_all_quotes() -> list[dict]:
-    BASE_URL = 'https://quotes.toscrape.com/'
-    quotes_html = fetch_html(BASE_URL)
-    quotes_data = parse_quotes_html(quotes_html)
+    BASE_URL = 'https://quotes.toscrape.com'
+    next_url_path = '/'
+    quotes_data = list()
+
+    while next_url_path:
+        print(f'pegou {next_url_path}')
+        quotes_html = fetch_html(BASE_URL + next_url_path)
+        quotes_data.extend(parse_quotes_html(quotes_html))
+        next_url_path = parse_next_url(quotes_html)
+        # if not next_url_path:
+        #     break
 
     return quotes_data
