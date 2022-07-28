edf8e430c4798a2e8de3adac486007ff33abfb0e
jpcp0614 <jpcp0614@gmail.com>
2022-07-28 16:39:08 -0300

    Exercise04

---
+++ ciencia-da-computacao/bloco-36-algoritmos/dia-2-recursividade-e-estrategias/exercises/exercise04.py
@@ -0,0 +1,14 @@
+'''
+Escreva um algoritmo recursivo para encontrar
+o máximo divisor comum (mdc) de dois inteiros.
+'''
+
+
+def mdc(a, b):
+    if b == 0:
+        return a
+    else:
+        return mdc(b, a % b)
+
+
+print(mdc(15, 10))
