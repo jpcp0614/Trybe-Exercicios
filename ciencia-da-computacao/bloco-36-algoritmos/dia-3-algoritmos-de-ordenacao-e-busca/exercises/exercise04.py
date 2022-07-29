60b3e67b18e805d0b706e33836b1ebd527317c9f
jpcp0614 <jpcp0614@gmail.com>
2022-07-29 13:59:52 -0300

    Exercise 04

--- ciencia-da-computacao/bloco-36-algoritmos/dia-3-algoritmos-de-ordenacao-e-busca/exercises/exercise04.py
+++ ciencia-da-computacao/bloco-36-algoritmos/dia-3-algoritmos-de-ordenacao-e-busca/exercises/exercise04.py
@@ -6,3 +6,18 @@ Explique através de análise de complexidade o que ocorre.
 
 
 from cronometro import Cronometro
+from mergeAndBubble import merge_sort, bubble_sort
+
+
+ordenados = list(range(10000))
+aleatorios = ordenados[:]
+
+with Cronometro("Merge Sort"):
+    merge_sort(aleatorios)
+
+with Cronometro("Bubble Sort"):
+    bubble_sort(aleatorios)
+
+
+# Merge demorou 0.0572 segundos
+# Bubble demorou 5.877 segundos
