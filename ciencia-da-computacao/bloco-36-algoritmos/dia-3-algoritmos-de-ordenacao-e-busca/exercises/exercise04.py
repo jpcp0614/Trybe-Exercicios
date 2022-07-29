06baeec0326737c77b1cf6e3085e0f27a8cb84ca
jpcp0614 <jpcp0614@gmail.com>
2022-07-29 13:50:29 -0300

    Renomeia diretório

---
+++ ciencia-da-computacao/bloco-36-algoritmos/dia-3-algoritmos-de-ordenacao-e-busca/exercises/exercise04.py
@@ -0,0 +1,8 @@
+'''
+Compare o tempo de execução do algoritmo merge_sort e bubble_sort
+para uma entrada de 10.000 valores aleatórios.
+Explique através de análise de complexidade o que ocorre.
+'''
+
+
+from cronometro import Cronometro
