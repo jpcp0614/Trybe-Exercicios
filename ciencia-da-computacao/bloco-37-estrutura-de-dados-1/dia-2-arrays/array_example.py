855a47c7704dd0a17007b5cf5eafddda034c55a2
jpcp0614 <jpcp0614@gmail.com>
2022-08-02 13:56:51 -0300

    Entendo a estrutura do array

--- ciencia-da-computacao/bloco-37-estrutura-de-dados-1/dia-2-arrays/array_example.py
+++ ciencia-da-computacao/bloco-37-estrutura-de-dados-1/dia-2-arrays/array_example.py
@@ -1,6 +1,7 @@
 """Perceba que temos uma coleção de valores
 e operações que atuam sobre estes valores,
 de acordo com o que foi definido pelo TAD."""
+import sys
 
 
 class Array:
@@ -35,16 +36,22 @@ array.set(0, "Felipe")
 array.set(1, "Ana")
 array.set(2, "Shirley")
 array.set(3, "Miguel")
+array.set(4, "Alberto")
+array.set(5, "Marta")
+array.set(6, "Túlio")
+array.set(7, "Michelle")
+array_memory_size = sys.getsizeof(array.data)
+print(array_memory_size)
 
 # para acessar um elemento do array, utilizamos seu índice
-print(array.get(0))  # saída: Felipe
-print(array.get(2))  # saída: Shirley
-print("-----")
+# print(array.get(0))  # saída: Felipe
+# print(array.get(2))  # saída: Shirley
+# print("-----")
 
 # podemos iterar sobre seus elementos da seguinte maneira
-index = 0
-# enquanto há elementos no array
-while index < len(array):
-    # recupera o elemento através de um índice
-    print("Index:", index, ", Nome:", array.get(index))
-    index += 1
+# index = 0
+# # enquanto há elementos no array
+# while index < len(array):
+#     # recupera o elemento através de um índice
+#     print("Index:", index, ", Nome:", array.get(index))
+#     index += 1
